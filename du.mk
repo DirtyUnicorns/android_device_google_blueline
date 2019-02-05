b43c6a2b441fe7fb89eafdf64d5fcbe307e2b5c7
Josh Chasky <jchasky@gmail.com>
2019-02-05 14:28:46 -0500

    Update build fingerprint to PQ2A.190205.001
    Change-Id: I6c54c30fcc8b14442d9ef560a01a997ba2078308

--- du.mk
+++ du.mk
@@ -39,8 +39,8 @@ PRODUCT_MANUFACTURER := Google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=blueline \
-    BUILD_FINGERPRINT=google/blueline/blueline:9/PQ1A.190105.004/5148680:user/release-keys \
-    PRIVATE_BUILD_DESC="blueline-user 9 PQ1A.190105.004 5148680 release-keys"
+    BUILD_FINGERPRINT=google/blueline/blueline:9/PQ2A.190205.001/5163636:user/release-keys \
+    PRIVATE_BUILD_DESC="blueline-user 9 PQ2A.190205.001 5163636 release-keys"
 
 $(call inherit-product-if-exists, vendor/google/blueline/blueline-vendor.mk)
 $(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
