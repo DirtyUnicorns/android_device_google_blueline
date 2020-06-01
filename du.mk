83b6f5e8670d014ed9ac37e1cf215b17ebb5e8b7
Josh Chasky <jchasky@gmail.com>
2020-06-01 16:43:36 -0400

    Update build fingerprint to QQ3A.200605.001
    Change-Id: I1da2fd821ef3604325ed72b8f86bad4fedf0aac6

--- du.mk
+++ du.mk
@@ -40,12 +40,12 @@ PRODUCT_MANUFACTURER := Google
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="blueline" \
     TARGET_DEVICE="blueline" \
-    PRIVATE_BUILD_DESC="blueline-user 10 QQ2A.200501.001.B2 6352890 release-keys"
+    PRIVATE_BUILD_DESC="blueline-user 10 QQ3A.200605.001 6392402 release-keys"
 
-BUILD_FINGERPRINT := "google/blueline/blueline:10/QQ2A.200501.001.B2/6352890:user/release-keys"
+BUILD_FINGERPRINT := "google/blueline/blueline:10/QQ3A.200605.001/6392402:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=google/blueline/blueline:10/QQ2A.200501.001.B2/6352890:user/release-keys
+    ro.build.fingerprint=google/blueline/blueline:10/QQ3A.200605.001/6392402:user/release-keys
 
 BOARD_PREBUILT_VENDORIMAGE := vendor/images/blueline/vendor.img
 
