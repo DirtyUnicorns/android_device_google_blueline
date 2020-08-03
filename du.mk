2b72477efee65c0263f98adbece77ee7a1c1b9e7
Josh Chasky <jchasky@gmail.com>
2020-08-03 17:13:39 -0400

    Update build fingerprint to QQ3A.200805.001
    Change-Id: Iba01aef012e7ca605ffee3921370d65e4e2c002e

--- du.mk
+++ du.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2018-2019 The Dirty Unicorns Project
+# Copyright (C) 2018-2020 The Dirty Unicorns Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -38,12 +38,12 @@ PRODUCT_MODEL := Pixel 3
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="blueline" \
-    PRIVATE_BUILD_DESC="blueline-user 10 QQ3A.200705.002 6506677 release-keys"
+    PRIVATE_BUILD_DESC="blueline-user 10 QQ3A.200805.001 6578210 release-keys"
 
-BUILD_FINGERPRINT := "google/blueline/blueline:10/QQ3A.200705.002/6506677:user/release-keys"
+BUILD_FINGERPRINT := "google/blueline/blueline:10/QQ3A.200805.001/6578210:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=google/blueline/blueline:10/QQ3A.200705.002/6506677:user/release-keys
+    ro.build.fingerprint=google/blueline/blueline:10/QQ3A.200805.001/6578210:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/blueline/blueline-vendor.mk)
 $(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
