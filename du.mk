54f6ff141166a3692da6fe2a4bebf8e2e5bd7223
Josh Chasky <jchasky@gmail.com>
2020-10-05 19:22:26 -0400

    Update build fingerprint to RP1A.201005.004
    Change-Id: Id3995888cd9ab16d0095d9b91185c699b4631d4f

--- du.mk
+++ du.mk
@@ -38,12 +38,12 @@ PRODUCT_MODEL := Pixel 3
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="blueline" \
-    PRIVATE_BUILD_DESC="blueline-user 11 RP1A.200720.009 6720564 release-keys"
+    PRIVATE_BUILD_DESC="blueline-user 11 RP1A.201005.004 6782484 release-keys"
 
-BUILD_FINGERPRINT := "google/blueline/blueline:11/RP1A.200720.009/6720564:user/release-keys"
+BUILD_FINGERPRINT := "google/blueline/blueline:11/RP1A.201005.004/6782484:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=google/blueline/blueline:11/RP1A.200720.009/6720564:user/release-keys
+    ro.build.fingerprint=google/blueline/blueline:11/RP1A.201005.004/6782484:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/blueline/blueline-vendor.mk)
 $(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
